948d8ee2e2e476c5f328fd67b22dc2849331e7f2
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2022-01-30 07:56:11 -0400

    Ajustes nos dados de associados

--- legado/portal-corporativo/src/app/sgps/associados/associado/associado.ts
+++ legado/portal-corporativo/src/app/sgps/associados/associado/associado.ts
@@ -1,6 +1,25 @@
 export interface Associado {
   id: number;
+  tipoPlano: string;
+  statusPlano: string;
+  tipoAcomodacao: string;
+  abrangencia: string;
+  dataInicioVigencia: Date;
+  dataFimVigencia: Date;
   nome: string;
   dataNascimento: string;
   sexo: string;
+  cpf: string;
+  endereco: {
+    cep: string;
+    uf: string;
+    logradouro: string;
+    municipio: {
+      id: string;
+      nome: string;
+    };
+    bairro: string;
+    numero: string;
+    complemento?: string;
+  };
 }
